e8a8d033543afd3a3db3f9eeb443e792873039bf
JJ <jj@jjkeith.net>
2016-06-07 09:45:45 -0700

    Add bootstrap gems

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+Card.create!(card_id: 'xy7-1', name: 'Oddish', types: 'Grass', imageUrl: 'https://s3.amazonaws.com/pokemontcg/xy7/1.png')
+Card.create!(card_id: 'xy7-19', name: 'Magikarp', types: 'Water', imageUrl: 'https://s3.amazonaws.com/pokemontcg/xy7/19.png')
+Card.create!(card_id: 'xy7-20', name: 'Gyarados', types: 'Water', imageUrl: 'https://s3.amazonaws.com/pokemontcg/xy7/20.png')
+Card.create!(card_id: 'xy7-26', name: 'Jolteon', types: 'Lightening', imageUrl: 'https://s3.amazonaws.com/pokemontcg/xy7/26.png')
+
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
